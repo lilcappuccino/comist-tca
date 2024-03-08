dfa2d441d6084e77f2ff50bd39071c18f7434efc
Vadim Marchenko <vadimmarchenko@Vadims-MacBook-Pro.local>
2024-03-08 14:56:08 +0200

    Initialize empty project with default configuration files

---
+++ Comist/ComistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComistApp.swift
+//  Comist
+//
+//  Created by Vadim Marchenko on 08.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ComistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
