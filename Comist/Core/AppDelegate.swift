7aaf850fb6ee60869f2b43e81758d5fe7ba9b480
Vadim Marchenko <vadimmarchenko@Vadims-MacBook-Pro.local>
2024-03-08 16:39:02 +0200

    Create configuration file for API keys and Firebase integration

---
+++ Comist/Core/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Comist
+//
+//  Created by Vadim Marchenko on 08.03.2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
