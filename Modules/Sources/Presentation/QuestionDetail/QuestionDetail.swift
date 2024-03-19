7ddf8166248ae6cd557f0f3abee7375475d97994
Vadim Marchenko <vadimmarchenko@Vadims-MacBook-Pro.local>
2024-03-19 22:58:38 +0200

    Detail screen was implemented

--- Modules/Sources/Presentation/QuestionDetail/QuestionDetail.swift
+++ Modules/Sources/Presentation/QuestionDetail/QuestionDetail.swift
@@ -9,37 +9,117 @@ import Foundation
 import SwiftUI
 import ComposableArchitecture
 import Core
+import Domain
 
 @Reducer
 public struct QuestionDetail {
-    
+
     // MARK: - Properties
     @Dependency(\.questionClient) var client
+    @Dependency(\.dismiss) var dismiss
     private enum CancelID { case client }
     private let logger = ComistLogger.questionDetailPresentation
-    
+
     // MARK: - Init
-    public init() { }
-    
+    public init() {}
+
     // MARK: - State
     @ObservableState
     public struct State {
-        public init() { }
+        public init(identifier: UUID,
+                    title: String = "") {
+            self.identifier = identifier
+            self.title = title
+        }
+        let title: String
+        let identifier: UUID
+        var selectedIndex = 0
+        var question: Question?
+
+        @Presents var alert: AlertState<Action.Alert>?
+        @Presents var addArgument: CreateArgument.State?
     }
-    
+
     // MARK: - Action
     public enum Action {
         case initialAction
+        case segmentTapped(Int)
+        case dismiss
+        case processResponse(Result<Question, Error>)
+        case alert(PresentationAction<Alert>)
+        case addArgument(PresentationAction<CreateArgument.Action>)
+        case addArgumentTapped
+        case itemDeleted(IndexSet)
+
+        public enum Alert: Equatable {
+            case dismissing
+        }
     }
-    
+
     // MARK: - Reducer
     public var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .initialAction:
+                return .run { [state] send in
+                    await send(.processResponse(Result { try await client.fetchItem(state.identifier) }))
+                }
+            case .segmentTapped(let index):
+                state.selectedIndex = index
                 return .none
+            case .alert(.presented(.dismissing)):
+                state.alert = nil
+                return .send(.dismiss)
+            case .dismiss:
+                return .run { _ in
+                    await dismiss()
+                }
+            case .processResponse(let response):
+                switch response {
+                case .success(let question):
+                    state.question = question
+                    return .none
+                case .failure:
+                    state.alert = AlertState {
+                        TextState("Opps...")
+                    } actions: {
+                        ButtonState(role: .cancel, action: .dismissing) {
+                            TextState("Ok")
+                        }
+                    } message: {
+                        TextState("Could not fetch item")
+                    }
+                    return .none
+                }
+            case .alert:
+                return .none
+            case .addArgument(.presented(.saved)):
+                return .send(.initialAction)
+            case .addArgument:
+                return .none
+            case .addArgumentTapped:
+                state.addArgument = CreateArgument.State(questionIdentifier: state.identifier)
+                return .none
+            case .itemDeleted(let indexSet):
+                let questionIdentifier = state.identifier
+                return .run { [state] send in
+                    for index in indexSet {
+                        do {
+                            if let argument = state.question?.arguments[index] {
+                                try await client.deleteArgument(questionIdentifier, argument.id)
+                            }
+                        } catch let error {
+                            logger.error("\(error)")
+                        }
+                    }
+                    await send(.initialAction)
+                }
             }
         }
+        .ifLet(\.$alert, action: \.alert)
+        .ifLet(\.$addArgument, action: \.addArgument) {
+            CreateArgument()
+        }
     }
-    
+
 }
--- Modules/Sources/Presentation/QuestionDetail/QuestionDetailView.swift
+++ Modules/Sources/Presentation/QuestionDetail/QuestionDetailView.swift
@@ -8,18 +8,117 @@
 import Foundation
 import SwiftUI
 import ComposableArchitecture
+import Domain
 
 public struct QuestionDetailView: View {
-    
+
     // MARK: - Properties
     @Bindable var store: StoreOf<QuestionDetail>
-    
+
     init(store: StoreOf<QuestionDetail>) {
         self.store = store
     }
-    
+
     // MARK: - Body
     public var body: some View {
-        Text("Test")
+        ZStack {
+            if let question = store.question {
+                content(question: question)
+            } else {
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .scaleEffect(CGSize(width: 2.0, height: 2.0))
+                    .navigationBarTitle(store.title)
+            }
+        }
+        .onAppear {
+            store.send(.initialAction)
+        }
+        .navigationBarTitle(store.title)
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                toolbar
+                    .foregroundStyle(Color.background)
+            }
+        }
+        .alert($store.scope(state: \.alert, action: \.alert))
+        .sheet(item: $store.scope(state: \.addArgument, action: \.addArgument)) { store in
+            CreateArgumentView(store: store)
+        }
     }
+
+    @ViewBuilder
+    var toolbar: some View {
+        HStack {
+            Spacer()
+            Button(action: {
+                store.send(.addArgumentTapped)
+            }, label: {
+                Image(systemName: "plus")
+                    .font(.title2)
+            })
+        }
+    }
+
+    func content(question: Question) -> some View {
+        VStack {
+            if let subtitle = question.subtitle {
+                Text(subtitle)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+            }
+
+            Picker("",
+                   selection:
+                    .init(get: { store.selectedIndex },
+                          set: { store.send(.segmentTapped($0))})
+            ) {
+                Text(prepareTitle(for: .pros))
+                    .tag(0)
+                Text(prepareTitle(for: .cons))
+                    .tag(1)
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            TabView(selection:
+                    .init(get: { store.selectedIndex },
+                          set: { store.send(.segmentTapped($0))})) {
+                generateForm(sum: question.prosArgumentsValue,
+                             arguments: question.prosArguments,
+                             position: .pros)
+                .tag(0)
+                generateForm(sum: question.consArgumentsValue,
+                             arguments: question.consArguments,
+                             position: .cons)
+                .tag(1)
+            }
+//                          .tabViewStyle(.page)
+//                          .disabled(true)
+        }
+    }
+
+    @ViewBuilder
+    func generateForm(sum: Int, arguments: [QuestionArgument], position: ArgumentPosition) -> some View {
+        Form {
+            Section("SUM: \(sum)") {
+                ForEach(arguments, id: \.id) { argument in
+                    Text("\(argument.value) ∘ \(argument.title)")
+                }
+                .onDelete(perform: { indexSet in
+                    store.send(.itemDeleted(indexSet))
+                })
+            }
+        }
+    }
+
+    func prepareTitle(for position: ArgumentPosition) -> String {
+        return "\(position.emoji) \(position.titleKey.localized)"
+    }
+}
+
+#Preview {
+    QuestionDetailView(store: Store(initialState: QuestionDetail.State(identifier: UUID())) {
+        QuestionDetail()
+    })
 }
